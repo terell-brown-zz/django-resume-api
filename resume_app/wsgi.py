741cff91f3414ebfb43f2fbbcce4a536a6064dff
young-Simba <terellbrown.93@gmail.com>
2015-08-21 17:43:33 -0600

    Add Requirements

--- resume_app/wsgi.py
+++ resume_app/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_app.settings")
 
 application = get_wsgi_application()
+
+try:
+	from django.core.wsgi import get_wsgi_application
+	from dj_static import Cling
+
+	application = Cling(get_wsgi_application())
+
+except:
+	pass
