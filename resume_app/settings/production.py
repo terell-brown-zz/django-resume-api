ca643466ab9bf4e2edca6f28328d660d9627f84e
young-Simba <terellbrown.93@gmail.com>
2015-08-22 00:57:16 -0600

    Added allowed host to production.py

--- resume_app/settings/production.py
+++ resume_app/settings/production.py
@@ -11,7 +11,8 @@ DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://stormy-depths-4606.herokuapp.com/',
+'stormy-depths-4606.herokuapp.com/','www.stormy-depths-4606.herokuapp.com/']
 
 # # Static asset configuration
 # import os
