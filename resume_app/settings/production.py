22372188431a5029338bfe7a42ba0593f520f198
young-Simba <terellbrown.93@gmail.com>
2015-08-21 23:39:27 -0600

    edit production settings

--- resume_app/settings/production.py
+++ resume_app/settings/production.py
@@ -5,7 +5,6 @@ from djano.conf import settings
 DEBUG = False
 TEMPLATE_DEBUG = True
 DATABASES = settings.DATABASES
-
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
@@ -14,7 +13,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
+# # Static asset configuration
 # import os
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = 'staticfiles'
