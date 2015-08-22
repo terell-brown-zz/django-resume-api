8f41e4608b824cf8f332c89073f2a725669f2df9
young-Simba <terellbrown.93@gmail.com>
2015-08-21 22:44:57 -0600

    Fixed settings folder

--- resume_app/wsgi.py
+++ resume_app/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_app.resume_app.settings")
 
 application = get_wsgi_application()
 
