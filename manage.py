b92c32831d7908805d496208bde73cedaebbb8f2
young-Simba <terellbrown.93@gmail.com>
2015-08-29 18:22:33 -0600

    updated allowed hosts

--- manage.py
+++ manage.py
@@ -4,9 +4,9 @@ import sys
 
 
 if __name__ == "__main__":
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_app.settings.production")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_app.settings.production")
     #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_app.settings")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_app.settings.local")
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_app.settings.local")
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resume_app.settings_local")
 
     from django.core.management import execute_from_command_line
