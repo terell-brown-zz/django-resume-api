4c5e103eed9bf535461913dbf9cbc8c346545e0c
young-Simba <terellbrown.93@gmail.com>
2015-08-29 21:13:19 -0600

    added everything to production.py

--- settings/production.py
+++ settings/production.py
@@ -2,10 +2,73 @@ import os
 import dj_database_url
 from django.conf import settings
 
-print 'production'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+LOGIN_URL ='/login/'
+LOGIN_REDIRECT_URL = '/resume/index'
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'builder',
+    'rest_framework',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'resume_app.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ["/".join((BASE_DIR,"resume_app/templates")),
+                "/".join((BASE_DIR,"builder/templates"))],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'resume_app.wsgi.application'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/Toronto'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+#SECRET_KEY = os.environ['SECRET_KEY']
 DATABASES = settings.DATABASES
 DATABASES['default'] =  dj_database_url.config()
 
